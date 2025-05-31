ac88022f6b576a8c561d4f6c056d51380d44e570
Reymark Labrador <reymark.l@homeofpmg.com>
2025-05-31 15:38:10 +0400

    profile update

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -2,7 +2,7 @@ export const profile = {
     firstName: 'Reymark',
     lastName: 'Labrador',
     position: 'Web Developer',
-    skills: 'React, Vue.js, Tailwind CSS | Wordpress & Statamic CMSLaravel & AWS Experience',
+    skills: 'React, Vue.js, Typescript, Tailwind CSS | PHP, Wordpress, Statamic CMS, Laravel, MYSQL & AWS Experience | C#, .NET, MSSQL',
 }
 
 export const getFullName = () => `${profile.firstName} ${profile.lastName}`
